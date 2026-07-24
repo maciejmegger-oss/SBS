84af7303e22ede3bf47b0096de27be00cef0d8b6
maciejmegger-oss <maciejmegger@gmail.com>
2026-07-24 11:41:41 +0200

    Disable HMR for production builds

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     port: 5173,
+    hmr: process.env.NODE_ENV === 'production' ? false : undefined,
+  },
+  build: {
+    sourcemap: false,
   },
 });
